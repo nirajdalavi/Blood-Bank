fcb8158c6ef5c860add8d336a4aa207a0ca7b56c
Niraj Dalavi <111214612+NirajDalavi@users.noreply.github.com>
2022-11-28 21:16:10 +0530

    Add files via upload

---
+++ bloodbank.sql
@@ -0,0 +1,117 @@
+create database if not exists bloodbank;
+use bloodbank;
+
+create table if not exists donor(
+    donor_id varchar(10) not null primary key,
+    donor_name varchar(30) not null,
+    donor_gender char(1) not null,
+    donor_dob DATE not null,
+    donor_bg varchar(3) not null,
+    donor_phone varchar(15) not null,
+    donor_address text not null
+);
+
+create table if not exists staff(
+    staff_id varchar(10) not null primary key,
+    staff_name varchar(30) not null,
+    staff_role varchar(15) not null,
+    staff_gender char(1) not null,
+    staff_dob date not null,
+    staff_bg varchar(3) not null,
+    staff_phone varchar(15) not null,
+    staff_address text not null
+);
+
+create table if not exists hospital(
+    hospital_id varchar(12) not null primary key,
+    hospital_name text not null,
+    hospital_phone varchar(10) not null,
+    hospital_address text not null
+);
+
+create table if not exists patient(
+    patient_id varchar(10) not null primary key,
+    patient_name varchar(30) not null,
+    patient_gender varchar(1) not null,
+    patient_dob date not null,
+    patient_bg varchar(3) not null,
+    patient_phone varchar(15) not null,
+    patient_address text not null
+);
+
+create table if not exists repository(
+    blood_id varchar(10) not null primary key,
+    blood_group varchar(3) not null,
+    RBC integer not null,
+    Platelets integer not null,
+    PLasma integer not null
+);
+
+create table if not exists RBC(
+    blood_id varchar(10) not null,
+    packet_id varchar(10) not null primary key,
+    ext_date date not null,
+    exp_date date not null,
+    foreign key(blood_id) references repository(blood_id) on delete cascade
+);
+
+create table if not exists Platelets(
+    blood_id varchar(10) not null,
+    packet_id varchar(10) not null primary key,
+    ext_date date not null,
+    exp_date date not null,
+    foreign key(blood_id) references repository(blood_id) on delete cascade
+);
+
+create table if not exists Plasma(
+    blood_id varchar(10) not null,
+    packet_id varchar(10) not null primary key,
+    ext_date date not null,
+    exp_date date not null,
+    foreign key(blood_id) references repository(blood_id) on delete cascade
+);
+
+create table if not exists donates(
+    donor_id varchar(10) not null,
+    staff_id varchar(10) not null, 
+    don_date date not null,
+    next_date date not null,
+    foreign key(donor_id) references donor(donor_id) on delete cascade,
+    foreign key(staff_id) references staff(staff_id) on delete cascade
+    
+);
+
+create table if not exists admits(
+    hospital_id varchar(10) not null,
+    patient_id varchar(10) not null,
+    patient_case varchar(30) not null,
+    date_of_admission date not null,
+    foreign key(hospital_id) references hospital(hospital_id) on delete cascade,
+    foreign key(patient_id) references patient(patient_id) on delete cascade
+);
+
+create table if not exists tested_by(
+    sample_id varchar(10) not null,
+    staff_id varchar(10) not null primary key,
+    result varchar(8) not null,
+    foreign key(staff_id) references staff(staff_id)
+);
+
+create table if not exists blood(
+    donor_id varchar(10) not null,
+    sample_id varchar(10) not null primary key,
+    foreign key(donor_id) references donor(donor_id) on delete cascade
+);
+
+create table if not exists req_rec(
+    hospital_id varchar(10) not null,
+    patient_id varchar(10) not null,
+    patient_case varchar(30) not null,
+    req_bg varchar(3) not null,
+    req_type varchar(10) not null,
+    qty integer not null,
+    foreign key(hospital_id) references hospital(hospital_id) on delete cascade,
+    foreign key(patient_id) references patient(patient_id) on delete cascade
+);
+
+show tables;
